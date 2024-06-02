e418677cf3f51befaf5f93a510b4226f06ee12ae
IgorBezusenko <bezusenko.98@mail.ru>
2024-06-02 20:04:10 +0300

    fix animation

--- js/script.js
+++ js/script.js
@@ -286,44 +286,20 @@ buttonAnimate?.forEach(item => item.addEventListener("click", handlerButtonAnima
 const animItems = document.querySelectorAll("._anim_items")
 
 if (animItems.length > 0) {
-    window.addEventListener("scroll", () => animOnScroll(false))
-    window.addEventListener("scrollend", () => animOnScroll(true))
+    window.addEventListener("scrollend", () => animOnScroll())
 
-    function animOnScroll(isScroll = false) {
+    function animOnScroll() {
         for (let i = 0; i < animItems.length; i++) {
             const animItem = animItems[i]
-            const animItemHeight = animItem.offsetHeight
-            const animItemOffset = offset(animItem).top
-            const animStart = 4
 
-            let animItemPoint = window.innerHeight - animItemHeight / animStart
-
-            if (animItemHeight > window.innerHeight) {
-                animItemPoint = window.innerHeight - window.innerHeight / animStart
-            }
-
-            if (!isScroll) {
-                if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
-                    animItem.classList.add("active")
-                } else {
-                    animItem.classList.remove("active")
-                }
-            } else {
+            setTimeout(() => {
                 animItem.classList.add("active")
-                setTimeout(() => {
-                    animItem.classList.remove("active")
-                }, 1200)
-            }
+            }, i * 150)
+            animItem.classList.remove("active")
 
 
         }
     }
 
-    function offset(el) {
-        const rect = el.getBoundingClientRect(),
-            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-        return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
-    }
 }
